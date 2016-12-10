1017c0f1a676194da8735f60f1baa8be0b7843c2
Kd Johar <kdjohar91@gmail.com>
2016-12-10 14:19:02 +0530

    model added for image upload

--- web/models.py
+++ web/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class ImageUpload(models.Model):
+    image = models.ImageField(upload_to='uploads/')
+
